917f348e74e708be093feb00b32cb38b7f9d23e8
alisher <alisher.sharipov2@Mail.ru>
2023-11-11 02:36:41 +0600

    Initial push

---
+++ src/main/java/com/example/domainTelegram/domainTelegram/DomainTelegramApplication.java
@@ -0,0 +1,13 @@
+package com.example.domainTelegram.domainTelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainTelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainTelegramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/domainTelegram/domainTelegram/DomainTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.domainTelegram.domainTelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainTelegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
